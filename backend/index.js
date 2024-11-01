8225ba5efe94643cbba2c7caf96a0233c5db23be
linuschui <linuschuii@gmail.com>
2024-11-02 00:36:04 +0800

    set up frontend

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,26 @@ const { Server } = require("socket.io")
 const http = require('http');
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "http://localhost:3000" } });
-
+io.on('connection', socket => {
+    console.log(`User connected: ${socket.id}`);
+    // join game
+    socket.on('joinGame', ({ sessionId, player }) => {
+        socket.join(sessionId);
+        io.to(sessionId).emit('message', `${player} has joined the game!`);
+    });
+    // make move
+    socket.on('makeMove', ({ sessionId, move }) => {
+       // to implement
+        io.to(sessionId).emit('moveMade', move);
+    });
+    // disconnect
+    socket.on('disconnect', () => {
+        console.log(`User disconnected: ${socket.id}`);
+    });
+});
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
 // DEFAULT ENDPOINT
 app.get("/", async (req, res) => {
     res.json({
